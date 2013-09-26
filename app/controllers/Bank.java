6513b53e2d65e3ec95458ac04606565f8321ec78
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-26 23:29:23 +0300

    Created basic Bank application that uses database structure

---
+++ app/controllers/Bank.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Bank extends Controller {
+    public static void overview() {
+        render();
+    }
+}
---
+++ app/models/Account.java
@@ -0,0 +1,20 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "account")
+public class Account extends Model {
+  @Column(name = "account_id") String accountId;
+  @Column(name = "balance") BigDecimal balance;
+
+	public Account() {
+		this.accountId = accountId;
+		this.balance = balance;
+	}
+}
