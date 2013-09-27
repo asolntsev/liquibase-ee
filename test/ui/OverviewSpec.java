6819815697eb7a3273d4bbfe625c9b5111b800df
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-28 00:46:34 +0300

    Also test account balance

--- test/ui/OverviewSpec.java
+++ test/ui/OverviewSpec.java
@@ -1,10 +1,14 @@
 package ui;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.Test;
 import play.test.UITest;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class OverviewSpec extends UITest {
@@ -12,5 +16,11 @@ public class OverviewSpec extends UITest {
   public void showsAccountsWithBalance() {
     open("/");
     $("h1").shouldHave(text("Bank overview"));
+
+    $$("#accounts .account").shouldHave(texts(
+        "acc-001-001-001 1000",
+        "acc-002-002-002 -202"));
+
+    holdBrowserOpen = true;
   }
 }
