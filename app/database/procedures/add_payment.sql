df3165f7f3f649db9f29c666514ae5ae21bb6ce4
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-10-08 22:45:53 +0300

    Added payment description; added test data for payments

--- app/database/procedures/add_payment.sql
+++ app/database/procedures/add_payment.sql
@@ -2,7 +2,8 @@ CREATE OR REPLACE PROCEDURE add_payment (
   account_from  IN VARCHAR2,
   account_to    IN VARCHAR2,
   amount        IN NUMBER,
-  payment_date  IN DATE
+  payment_date  IN DATE,
+  description   IN VARCHAR2
 )
 IS
   v_payment_type VARCHAR2(6) := 'DEBIT';
@@ -14,5 +15,5 @@ BEGIN
   END IF;
 
   INSERT INTO payment
-  VALUES (payment_id_seq.nextval, v_payment_type, account_from, account_to, amount, payment_date);
+  VALUES (payment_id_seq.nextval, v_payment_type, account_from, account_to, amount, payment_date, description);
 END add_payment;
--- app/models/Payment.java
+++ app/models/Payment.java
@@ -11,9 +11,10 @@ import java.util.Date;
 @Entity
 @Table(name = "payment")
 public class Payment extends Model {
-  @Column(name = "payment_type") String type;
-  @Column(name = "account_from") String accountFrom;
-  @Column(name = "account_to") String accountTo;
-  @Column(name = "amount") BigDecimal balance;
-  @Column(name = "payment_date") Date date;
+  @Column(name = "payment_type") public String type;
+  @Column(name = "account_from") public String accountFrom;
+  @Column(name = "account_to") public String accountTo;
+  @Column(name = "amount") public BigDecimal amount;
+  @Column(name = "payment_date") public Date date;
+  @Column public String description;
 }
